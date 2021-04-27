dc32133ce2caee63ed075ec59d00ecd4964e8368
Enzo-Edele <78355035+Enzo-Edele@users.noreply.github.com>
2021-04-27 16:29:12 +0200

    Cadi V1

---
+++ ProjetCadieDeDavid/Assets/Script/Articles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Articles : MonoBehaviour
+{
+    public string nom;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ ProjetCadieDeDavid/Assets/Script/Caddie.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caddie : MonoBehaviour
+{
+    BoxCollider2D bc2d;
+    int capacité;
+    int capacitéMax = 20;
+
+    float speed = 20;
+    float horizontal;
+    float vertical;
+    void Start()
+    {
+        bc2d = GetComponent<BoxCollider2D>();
+        capacité = capacitéMax;
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        transform.position = position;
+ 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Articles recup = collision.GetComponent<Articles>();
+        capacité -= 1;
+        Vector2 box = bc2d.size;
+        box.y += 0.1f;
+        bc2d.size = box;
+        Vector2 offset = bc2d.offset;
+        offset.y += 0.03f;
+        bc2d.offset = offset;
+    }
+}
