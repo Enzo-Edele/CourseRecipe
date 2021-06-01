6ea5ef6d328087bc98063bbdf7acb8e492b7b773
Frédéric Deiber <frederic.deiber@gmail.com>
2021-06-01 16:06:18 +0200

    Recettes

--- ProjetCadieDeDavid/Assets/Script/BookManager.cs
+++ ProjetCadieDeDavid/Assets/Script/BookManager.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.IO;
 
 public class BookManager : MonoBehaviour
 {
     int levelProgress;
 
-    public GameObject listeRecetteUn;
-    public GameObject listeRecetteDeux;
-    public GameObject listeRecetteTrois;
-    public GameObject listeRecetteQuatre;
-    public GameObject listeRecetteCinq;
-    public GameObject listeRecetteSix;
-    public GameObject listeRecetteSept;
-    public GameObject listeRecetteHuit;
-    public GameObject listeRecetteNeuf;
-    public GameObject listeRecetteDix;
-    public GameObject listeRecetteOnze;
-    public GameObject listeRecetteDouze;
-    public GameObject listeRecetteTreize;
+    public GameObject[] arrayRecette;
 
     public GameObject listRecette;
     public TMP_Text levelText;
@@ -29,19 +18,6 @@ public class BookManager : MonoBehaviour
     public GameObject etoileUn, etoileDeux, etoileTrois;
     public TMP_Text ticketText;
 
-    readonly string recetteUn = "Recette Un";
-    readonly string recetteDeux = "Recette Deux";
-    readonly string recetteTrois = "Recette Trois";
-    readonly string recetteQuatre = "Recette Quatre";
-    readonly string recetteCinq = "Recette Cinq";
-    readonly string recetteSix = "Recette Six";
-    readonly string recetteSept = "Recette Sept";
-    readonly string recetteHuit = "Recette Huit";
-    readonly string recetteNeuf = "Recette Neuf";
-    readonly string recetteDix = "Recette Dix";
-    readonly string recetteOnze = "Recette Onze";
-    readonly string recetteDouze = "Recette Douze";
-    readonly string recetteTreize = "Recette Treize";
     
     public TMP_Text recette;
     public GameObject ImageRecette;
@@ -141,61 +117,8 @@ public class BookManager : MonoBehaviour
         {
             SoundManagerBehaviour.instance.PlayButtonBackSound();
         }
-        switch (level)
-        {
-            case 1:
-                listRecette = listeRecetteUn;
-                recette.text = recetteUn;
-                break;
-            case 2:
-                listRecette = listeRecetteDeux;
-                recette.text = recetteDeux;
-                break;
-            case 3:
-                listRecette = listeRecetteTrois;
-                recette.text = recetteTrois;
-                break;
-            case 4:
-                listRecette = listeRecetteQuatre;
-                recette.text = recetteQuatre;
-                break;
-            case 5:
-                listRecette = listeRecetteCinq;
-                recette.text = recetteCinq;
-                break;
-            case 6:
-                listRecette = listeRecetteSix;
-                recette.text = recetteSix;
-                break;
-            case 7:
-                listRecette = listeRecetteSept;
-                recette.text = recetteSept;
-                break;
-            case 8:
-                listRecette = listeRecetteHuit;
-                recette.text = recetteHuit;
-                break;
-            case 9:
-                listRecette = listeRecetteNeuf;
-                recette.text = recetteNeuf;
-                break;
-            case 10:
-                listRecette = listeRecetteDix;
-                recette.text = recetteDix;
-                break;
-            case 11:
-                listRecette = listeRecetteOnze;
-                recette.text = recetteOnze;
-                break;
-            case 12:
-                listRecette = listeRecetteDouze;
-                recette.text = recetteDouze;
-                break;
-            case 13:
-                listRecette = listeRecetteTreize;
-                recette.text = recetteTreize;
-                break;
-        }
+        listRecette = arrayRecette[level -1];
+        recette.text = System.IO.File.ReadAllText("Assets/Recettes/Recipe" + level + ".txt");
         anim.SetTrigger("turnPage");
         inUseRecette.sprite = newImageRecette[level - 1];
         GameManagerBehaviour.instance.levelSelect = level;
