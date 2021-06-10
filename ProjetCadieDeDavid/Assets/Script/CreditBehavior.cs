2a5735a801e32b5b8a318ec452a861c4b8a61063
Enzo-Edele <78355035+Enzo-Edele@users.noreply.github.com>
2021-06-10 16:40:31 +0200

    Credit debut

---
+++ ProjetCadieDeDavid/Assets/Script/CreditBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditBehavior : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public GameObject thank;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.y = position.y + speed;
+        transform.position = position;
+        Vector2 positionThank = thank.transform.position;
+        if(positionThank.y < Screen.height * 0.5f)
+        {
+            positionThank.y = positionThank.y + speed;
+            thank.transform.position = positionThank;
+        }
+    }
+}
